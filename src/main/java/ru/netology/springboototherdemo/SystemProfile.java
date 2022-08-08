c0c25af70661d6e22759ce983abd9df9eb984479
Evgeniy Kov <yrec_92@mail.ru>
2022-08-08 14:27:35 +0400

    Initial commit

---
+++ src/main/java/ru/netology/springboototherdemo/SystemProfile.java
@@ -0,0 +1,5 @@
+package ru.netology.springboototherdemo;
+
+public interface SystemProfile {
+    String getProfile();
+}
---
+++ src/test/java/ru/netology/springboototherdemo/SpringBootOtherDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.netology.springboototherdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootOtherDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
